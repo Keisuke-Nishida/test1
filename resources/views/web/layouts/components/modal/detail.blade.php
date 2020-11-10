017eed6df72707f1f9344c144fd358612b8239e4
ako-fa <shotaro.ako@frontarc.co.jp>
2020-11-10 18:46:25 +0900

    [add] ファイルの追加
    管理画面で使用する送り先の詳細モーダル用のファイルを追加

---
+++ resources/views/web/layouts/components/modal/detail.blade.php
@@ -0,0 +1,43 @@
+{{-- Modal --}}
+<div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-labelledby="detailModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header content__header__background-color">
+                <h5 class="modal-title" id="detailModalLabel">(有)Alpha種苗</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="main-list__table table table-borderless table-sm mb-0 bg-transparent">
+                    <tbody class="main-list__tbody">
+                        <tr>
+                            <th>住所</th>
+                            <td>○○○○○○市○○－○○－○○</td>
+                            <th>郵便番号</th>
+                            <td>012-3456</td>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td>○○○○○○○○○○○○○○○</td>
+                            <th>電話番号</th>
+                            <td>00-0000-0000</td>
+                        </tr>
+                        <tr>
+                            <th>気付</th>
+                            <td>○○○○○部○○○○○○○○○　様</td>
+                        </tr>
+                        <tr>
+                            <th>送り先名</th>
+                            <td>株式会社○○○○○○○○○○○　様</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
