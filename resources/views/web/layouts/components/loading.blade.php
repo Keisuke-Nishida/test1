80d98316fe854439b64f8796c743d58bb35e9b26
ako-fa <shotaro.ako@frontarc.co.jp>
2020-11-10 18:45:12 +0900

    [add] ローディング画面用のbladeファイルを追加

---
+++ resources/views/web/layouts/components/loading.blade.php
@@ -0,0 +1,12 @@
+{{-- Loding Spinner --}}
+<div id="loading_bg" class="">
+    <div id="loading_spinner" class="">
+        <div class="spinner-border text__main" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="text-center">
+            <br>
+            <strong>処理中です。しばらくお待ち下さい。</strong>
+        </div>
+    </div>
+</div>
