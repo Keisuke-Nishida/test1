2c59ac201f1c278355a8455dfac722deff87e92a
ako-fa <shotaro.ako@frontarc.co.jp>
2020-12-04 14:10:34 +0900

    [update] LoginControllerでguardの指定方法を変更
    クラス内のgurad()を使用

--- app/Http/Controllers/Web/Auth/LoginController.php
+++ app/Http/Controllers/Web/Auth/LoginController.php
@@ -132,7 +132,7 @@ class LoginController extends Controller
      */
     public function logout(Request $request)
     {
-        Auth::guard('web')->logout();
+        $this->guard()->logout();
 
         return $this->loggedOut($request);
     }
@@ -180,10 +180,10 @@ class LoginController extends Controller
         }
         $request->session()->regenerate();
         $this->clearLoginAttempts($request);
-        $cookies = \Auth::getCookieJar();
-        $value = $cookies->queued(\Auth::getRecallerName())->getValue();
+        $cookies = $this->guard()->getCookieJar();
+        $value = $cookies->queued($this->guard()->getRecallerName())->getValue();
         // ログイン保存の時間を変更
-        $cookies->queue(\Auth::getRecallerName(), $value, Constant::REMEMBER_TOKEN_TIME);
+        $cookies->queue($this->guard()->getRecallerName(), $value, Constant::REMEMBER_TOKEN_TIME);
 
         return $this->authenticated($request, $this->guard()->user())
             ?: redirect()->intended($this->redirectPath());
