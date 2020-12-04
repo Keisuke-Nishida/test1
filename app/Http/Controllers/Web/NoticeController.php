dd5db1b5179c56d543dbe881f1d94047b706dc82
ako-fa <shotaro.ako@frontarc.co.jp>
2020-12-04 15:34:58 +0900

    [add] お知らせ一覧画面の表示機能追加

--- app/Http/Controllers/Web/NoticeController.php
+++ app/Http/Controllers/Web/NoticeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Services\Models\NoticeDataService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
 /**
  * お知らせ管理コントローラー
  * Class NoticeController
@@ -9,11 +13,37 @@ namespace App\Http\Controllers\Web;
  */
 class NoticeController extends BaseController
 {
+    // 一覧に表示する文字数制限
+    protected $limit_title_length = 60;
+    protected $limit_body_length = 200;
 
-    public function __construct()
+    public function __construct(NoticeDataService $noticeDataService)
     {
         parent::__construct();
-        $this->mainService = null;
-        $this->mainRoot     = "web/notice";
+        $this->mainService = $noticeDataService;
+        $this->mainRoot    = "web/notice";
+    }
+
+    /**
+     * 一覧画面
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        // 現在時刻の取得
+        $current_time = Carbon::now();
+        // 検索条件
+        // 配信終了時刻より現在時刻が小さいものを取得する
+        $conditions["notice_data.end_time@>"] = $current_time;
+
+        // ソート順
+        $order = ["start_time" => "desc"];
+
+        $list = $this->mainService->searchPaginate($conditions, $order, [], $this->listPerLink);
+        return parent::index($request)->with([
+            'list' => $list,
+            'limit_title_length' => $this->limit_title_length,
+            'limit_body_length'  => $this->limit_body_length,
+        ]);
     }
 }
--- resources/views/web/notice/index.blade.php
+++ resources/views/web/notice/index.blade.php
@@ -1,229 +1,69 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('web.layouts.app')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+{{-- 追加CSS --}}
+@section('app_css')
+@endsection
 
-    <!-- CSS -->
-    <!-- bootstrap4用 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-    <!-- dataTables -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.7/css/fixedHeader.bootstrap4.min.css" />
-    <!-- datetimepicker -->
-    <link rel="stylesheet" href="../../../public/css/datetimepicker/jquery.datetimepicker.min.css">
-    <!-- 共通 -->
-    <link rel="stylesheet" href="../../../public/css/style.css" />
-    <!-- 列固定テーブル -->
-    <link rel="stylesheet" href="../../../public/css/fixed.table.css" />
 
-    <!-- fontawesome -->
-    <!-- cdn廃止予定のためdownload版を使用 -->
-    <link rel="stylesheet" href="../../../public/fontawesome-free-5.14.0-web/css/all.css" />
+@section('app_title')
+    お知らせ一覧
+@endsection
 
-    <title>掲示板一覧 | タキイ Web情報閲覧サービス</title>
-</head>
-
-<body class="page">
-
-    <!-- ヘッダー -->
-    <header class="header sticky-top">
-        <div class="header__bachground-image">
-            <div class="header__bachground-image__mask">
-                <div class="d-flex flex-row align-items-center px-5 py-3 shadow">
-                    <!-- ロゴ -->
-                    <div class="mr-auto">
-                        <a class="header__logo my-0 mr-lg-auto" href="../home/index.blade.html">
-                            <img src="../../../public/images/LOGO.png" alt="LOGO画像">
-                        </a>
-                    </div>
-
-                    <!-- ログイン表示/会員メニュー -->
-                    <div class="d-flex flex-column flex-lg-row ml-5">
-                        <div class="header__text d-flex align-items-center">
-                            ログイン中：株式会社○○○○○○○○○○○○(ID:123456789) 様
-                        </div>
-                        <div class="d-flex justify-content-end mt-2 mt-lg-0 ml-0 ml-lg-3">
-                            <div class="btn-group" role="group">
-                                <button id="btnGroupDrop1" type="button"
-                                    class="btn button__main dropdown-toggle btn-sm px-3" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    会員メニュー
-                                </button>
-                                <div class="header__dropdown-menu dropdown-menu dropdown-menu-right"
-                                    aria-labelledby="btnGroupDrop1">
-                                    <a class="dropdown-item header__dropdown-menu__text header__dropdown-menu__item"
-                                        href="../mypage/password.blade.html"><i class="fa fa-key"
-                                            aria-hidden="true"></i>
-                                        パスワード変更</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item header__dropdown-menu__text header__dropdown-menu__item"
-                                        href="../auth/login.blade.html"><i class="fa fa-lock" aria-hidden="true"></i>
-                                        ログアウト</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!-- Breadcrumb -->
+@section('app_bread')
+    <div class="row mt-2">
+        <div class="page__breadcrumb col col-lg-10 d-flex align-items-end">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent mb-0">
+                    <li class="breadcrumb-item">
+                        <a class="link__main" href="/">HOME</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        お知らせ一覧
+                    </li>
+                </ol>
+            </nav>
         </div>
-    </header>
+    </div>
+@endsection
 
-    <div class="container mb-5">
-
-        <!-- パンくずリスト -->
-        <div class="row mt-2">
-            <!-- ページタイトル -->
-            <!-- <div class="col col-lg-2 d-flex align-items-end">
-            <h1 class="page__title">HOME</h1>
-        </div> -->
-            <div class="page__breadcrumb col col-lg-10 d-flex align-items-end">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0">
-                        <li class="breadcrumb-item">
-                            <a class="link__main" href="../home/index.blade.html">HOME</a>
-                        </li>
-                        <li class="breadcrumb-item active" aria-current="page">
-                            お知らせ一覧
-                        </li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-
-        <hr class="page__hr my-0">
-
-        <!-- row -->
-        <div class="row article">
-            <div class="col py-3">
-                <div class="content bg-white shadow content__card--round p-4">
-                    <ul class="article-list list-unstyled">
-                        <a class="text-decoration-none" href="./detail.blade.html">
+<!-- コンテンツ -->
+@section('app_contents')
+    <div class="row article">
+        <div class="col py-3">
+            <div class="content bg-white shadow content__card--round p-4">
+                <ul class="article-list list-unstyled">
+                    @foreach ($list as $data)
+                        <a class="text-decoration-none" href="/notice/detail/{{ $data->id }}">
                             <li class="article-list__item media p-4">
                                 <div class="media-body">
-                                    <h3 class="article-list__title m-0"><u>湖南市市民産業交流施設「ここぴあ」オープンの模様をご紹介いたします</u></h3>
-                                    <small class="article-list__date"><i class="far fa-clock fa-fw"></i>
-                                        2020.01.01</small>
-                                    <p class="article-list__text mb-0 text-secondary" style="font-size: 0.9em;">
-                                        本誌『タキイ最前線』2017年秋種特集号より連載スタートした「ここぴあ通信」滋賀県湖南市の市民産業交流施設「ここぴあ」内の直売所へ...
+                                    <h3 class="article-list__title m-0">
+                                        <u>{{ Str::limit($data->title, $limit_title_length) }}</u>
+                                    </h3>
+                                    <small class="article-list__date d-block mt-3">
+                                        <i class="far fa-clock fa-fw"></i>{{ $data->start_time }}
+                                    </small>
+                                    <p class="article-list__text mb-0 text-secondary mt-3" style="font-size: 0.9em;">
+                                        {{ Str::limit($data->body, $limit_body_length) }}
                                     </p>
                                 </div>
                             </li>
                         </a>
-                        <a class="text-decoration-none" href="./detail.blade.html">
-                            <li class="article-list__item media p-4 mt-3">
-                                <div class="media-body">
-                                    <h3 class="article-list__title m-0"><u>湖南市市民産業交流施設「ここぴあ」オープンの模様をご紹介いたします</u></h3>
-                                    <small class="article-list__date"><i class="far fa-clock fa-fw"></i>
-                                        2020.01.01</small>
-                                    <p class="article-list__text mb-0 text-secondary" style="font-size: 0.9em;">
-                                        本誌『タキイ最前線』2017年秋種特集号より連載スタートした「ここぴあ通信」滋賀県湖南市の市民産業交流施設「ここぴあ」内の直売所へ...
-                                    </p>
-                                </div>
-                            </li>
-                        </a>
-                        <a class="text-decoration-none" href="./detail.blade.html">
-                            <li class="article-list__item media p-4 mt-3">
-                                <div class="media-body">
-                                    <h3 class="article-list__title m-0"><u>湖南市市民産業交流施設「ここぴあ」オープンの模様をご紹介いたします</u></h3>
-                                    <small class="article-list__date"><i class="far fa-clock fa-fw"></i>
-                                        2020.01.01</small>
-                                    <p class="article-list__text mb-0 text-secondary" style="font-size: 0.9em;">
-                                        本誌『タキイ最前線』2017年秋種特集号より連載スタートした「ここぴあ通信」滋賀県湖南市の市民産業交流施設「ここぴあ」内の直売所へ...
-                                    </p>
-                                </div>
-                            </li>
-                        </a>
-                        <a class="text-decoration-none" href="./detail.blade.html">
-                            <li class="article-list__item media p-4 mt-3">
-                                <div class="media-body">
-                                    <h3 class="article-list__title m-0"><u>湖南市市民産業交流施設「ここぴあ」オープンの模様をご紹介いたします</u></h3>
-                                    <small class="article-list__date"><i class="far fa-clock fa-fw"></i>
-                                        2020.01.01</small>
-                                    <p class="article-list__text mb-0 text-secondary" style="font-size: 0.9em;">
-                                        本誌『タキイ最前線』2017年秋種特集号より連載スタートした「ここぴあ通信」滋賀県湖南市の市民産業交流施設「ここぴあ」内の直売所へ...
-                                    </p>
-                                </div>
-                            </li>
-                        </a>
-                        <a class="text-decoration-none" href="./detail.blade.html">
-                            <li class="article-list__item media p-4 mt-3">
-                                <div class="media-body">
-                                    <h3 class="article-list__title m-0"><u>湖南市市民産業交流施設「ここぴあ」オープンの模様をご紹介いたします</u></h3>
-                                    <small class="article-list__date"><i class="far fa-clock fa-fw"></i>
-                                        2020.01.01</small>
-                                    <p class="article-list__text mb-0 text-secondary" style="font-size: 0.9em;">
-                                        本誌『タキイ最前線』2017年秋種特集号より連載スタートした「ここぴあ通信」滋賀県湖南市の市民産業交流施設「ここぴあ」内の直売所へ...
-                                    </p>
-                                </div>
-                            </li>
-                        </a>
-                    </ul>
+                    @endforeach
+                </ul>
 
-                    <!-- ページネーション -->
-                    <div class="content__bottom__link text-center">
-                        <div class="paging_simple_numbers" id="main_list_paginate">
-                            <ul class="pagination justify-content-center justify-content-md-end">
-                                <li class="paginate_button page-item previous disabled" id="main_list_previous">
-                                    <a href="#" aria-controls="main_list" data-dt-idx="0" tabindex="0"
-                                        class="page-link">前</a>
-                                </li>
-                                <li class="paginate_button page-item active">
-                                    <a href="#" aria-controls="main_list" data-dt-idx="1" tabindex="0"
-                                        class="page-link">1</a>
-                                </li>
-                                <li class="paginate_button page-item">
-                                    <a href="#" aria-controls="main_list" data-dt-idx="2" tabindex="0"
-                                        class="page-link">2</a>
-                                </li>
-                                <li class="paginate_button page-item">
-                                    <a href="#" aria-controls="main_list" data-dt-idx="3" tabindex="0"
-                                        class="page-link">3</a>
-                                </li>
-                                <li class="paginate_button page-item next" id="main_list_next">
-                                    <a href="#" aria-controls="main_list" data-dt-idx="4" tabindex="0"
-                                        class="page-link">次</a>
-                                </li>
-                            </ul>
-                        </div>
+                <!-- ページネーション -->
+                <div class="content__bottom__link text-center">
+                    <div class="content__bottom__link pagination justify-content-center justify-content-md-end">
+                        {{ $list->onEachSide(1)->links('web.layouts.components.pagination.custom') }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <footer class="footer">
-        <div class="contariner">
-            <div class="row m-0">
-                <div class="col text-center p-2">
-                    <small class="footer__text text-white">Copyright © TAKII & CO.,LTD.</small>
-                </div>
-            </div>
-        </div>
-    </footer>
+@endsection
 
-    <!-- JavaScript -->
-    <!-- jQuery本体 -->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"
-        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-        crossorigin="anonymous"></script>
-    <!-- dataTables -->
-    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdn.datatables.net/fixedheader/3.1.7/js/dataTables.fixedHeader.min.js"></script>
-    <!-- datetimepicker -->
-    <script src="../../../public/js/datetimepicker/jquery.datetimepicker.full.min.js"></script>
-    <!-- 共有 -->
-    <script src="../../../public/js/main.js"></script>
-</body>
-
-</html>
+{{-- 追加JSファイル --}}
+@section('app_js')
+@endsection
