640e947f0dc3c7a857e97d26e63ec010d9e6149f
ako-fa <shotaro.ako@frontarc.co.jp>
2020-12-07 11:03:56 +0900

    [add] 掲示板詳細画面の表示とファイルのダウンロード機能追加

--- app/Http/Controllers/Web/BulletinBoardController.php
+++ app/Http/Controllers/Web/BulletinBoardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Services\Models\BulletinBoardDataService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * 掲示板コントローラー
@@ -45,5 +46,36 @@ class BulletinBoardController extends BaseController
             'limit_title_length' => $this->limit_title_length,
             'limit_body_length'  => $this->limit_body_length,
         ]);
+    }
+
+    /**
+     * 詳細
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function detail($id)
+    {
+        $data = $this->mainService->find($id);
+        return parent::detail($id)->with([
+            'data' => $data,
+            'limit_title_length' => $this->limit_title_length,
+        ]);
+    }
+
+    /**
+     * ファイルダウンロード
+     *
+     * @param  mixed $id
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function download($id)
+    {
+        $prefix = "public/files/";
+        $data = $this->mainService->find($id);
+        $file_name = $data->file_name;
+        $file_path = $prefix . $file_name;
+        $mime_type = Storage::mimeType($file_path);
+        $headers = [["Content-Type" => $mime_type]];
+
+        return Storage::download($file_path, $file_name, $headers);
     }
 }
