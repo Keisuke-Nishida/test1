af80045957811f79c4b6908d4db038409ddb6db8
ako-fa <shotaro.ako@frontarc.co.jp>
2020-12-08 11:30:26 +0900

    [add / delete] ファイル整理
    UserAgreeDateApiServiceの処理をUserAgreeDataServiceに移動し、UserAgreeDateApiServiceファイルの削除

--- app/Services/Models/UserAgreeDataService.php
+++ app/Services/Models/UserAgreeDataService.php
@@ -19,4 +19,32 @@ class UserAgreeDataService extends BaseService
     {
         $this->model = new UserAgreeData();
     }
+
+    /**
+     * getUserAgreeData
+     * ユーザーIDから同意履歴のデータを取得する
+     *
+     * @param  mixed $user_id
+     * @return mixed
+     */
+    public function getUserAgreeData($user_id)
+    {
+        $conditions = ["user_id" => $user_id];
+        $user_agree_data = $this->searchOne($conditions);
+
+        return $user_agree_data;
+    }
+
+    /**
+     * isAgreementHistory
+     * getUserDataFromIdAndPassword()で取得したデータを使用して
+     * user_agree_dataテーブルにユーザの同意履歴のデータの有無を返却する
+     *
+     * @param  mixed $user_data
+     * @return bool
+     */
+    public function isAgreementHistory($user_data): bool
+    {
+        return $this->searchExists(["user_id" => $user_data->id]);
+    }
 }
